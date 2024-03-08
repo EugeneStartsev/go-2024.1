8556bbf027225e065d5be8536a8e0c0d5f38fde4
Старцев Евгений <startsev@corp.sputnik.ru>
2024-03-08 16:27:14 +0300

    Добавить проверку на нулевую длину слайса в функцию RemoveElement

--- internal/slice/slice.go
+++ internal/slice/slice.go
@@ -8,14 +8,18 @@ func AppendElement(ints []int, elem int) []int {
 
 // RemoveElement удаляет последний элемент из слайса. Если массив пуст, функция возвращает оригинальный пустой массив.
 func RemoveElement(ints []int) []int {
-	ints = ints[:len(ints)-1]
+	if len(ints) != 0 {
+		ints = ints[:len(ints)-1]
+	}
+
 	return ints
 }
 
 // AddOneToAll увеличивает каждый элемент массива на единицу.
 func AddOneToAll(ints []int) []int {
-	for i := 0; i < len(ints); i++ {
-		ints[i] += 1
+	for i, v := range ints {
+		ints[i] = v + 1
 	}
+
 	return ints
 }
