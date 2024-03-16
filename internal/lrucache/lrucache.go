bebfe8f287d8cbaa503cc32bf8001860d2f1bed1
Старцев Евгений <startsev@corp.sputnik.ru>
2024-03-16 20:25:48 +0300

    Реализовать функции Set, Get, Range, Clear для lruCache struct

--- internal/lrucache/lrucache.go
+++ internal/lrucache/lrucache.go
@@ -1,5 +1,68 @@
 package lrucache
 
+import (
+	"container/list"
+	"time"
+)
+
+type lruCache struct {
+	capacity int
+	items    map[int]*list.Element
+	queue    *list.List
+	time     time.Time
+}
+
 func New(cap int) Cache {
-	panic("implement me")
+	return &lruCache{
+		capacity: cap,
+		items:    make(map[int]*list.Element),
+		queue:    list.New(),
+	}
+}
+
+func (l *lruCache) Get(key int) (int, bool) {
+	elem, isExist := l.items[key]
+
+	if isExist == false {
+		return 0, false
+	}
+
+	l.queue.MoveToFront(elem)
+	l.time = time.Now()
+	return elem.Value.(int), true
+}
+
+func (l *lruCache) Set(key, value int) {
+	if elem, isExist := l.items[key]; isExist == true {
+		l.queue.MoveToFront(elem)
+		elem.Value = value
+		l.time = time.Now()
+	}
+
+	if l.queue.Len() == l.capacity {
+		l.purge()
+	}
+
+	elem := l.queue.PushFront(value)
+	l.items[key] = elem
+	l.time = time.Now()
+}
+
+func (l *lruCache) Range(f func(key, value int) bool) {
+	for key, value := range l.items {
+		f(key, value.Value.(int))
+	}
+}
+
+func (l *lruCache) Clear() {
+	for len(l.items) > 0 {
+		l.purge()
+	}
+}
+
+func (l *lruCache) purge() {
+	if elem := l.queue.Back(); elem != nil {
+		item := l.queue.Remove(elem).(int)
+		delete(l.items, item)
+	}
 }
