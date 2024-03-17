384dc9ecd2c646c4baacee0854581a87b6b59809
Старцев Евгений <startsev@corp.sputnik.ru>
2024-03-17 16:11:03 +0300

    Изменить SET и RANGE

--- lrucache/lrucache.go
+++ lrucache/lrucache.go
@@ -2,14 +2,12 @@ package lrucache
 
 import (
 	"container/list"
-	"sort"
-	"time"
 )
 
 type Item struct {
-	key       int
-	value     int
-	timestamp time.Time
+	key   int
+	value int
+	//timestamp time.Time
 }
 
 type lruCache struct {
@@ -33,58 +31,65 @@ func (l *lruCache) Get(key int) (int, bool) {
 		return 0, false
 	}
 
-	elem.Value.(*Item).timestamp = time.Now()
 	l.queue.MoveToFront(elem)
 	return elem.Value.(*Item).value, true
 }
 
 func (l *lruCache) Set(key, value int) {
-	if l.capacity > 0 {
-		if elem, isExist := l.items[key]; isExist == true {
-			l.queue.MoveToFront(elem)
-			elem.Value.(*Item).value = value
-			elem.Value.(*Item).timestamp = time.Now()
-		}
-
-		if l.queue.Len() == l.capacity {
-			l.purge()
-		}
-
-		item := &Item{
-			key:       key,
-			value:     value,
-			timestamp: time.Now(),
-		}
-
-		elem := l.queue.PushFront(item)
-		l.items[key] = elem
+	if l.capacity == 0 {
+		return
 	}
+
+	if elem, isExist := l.items[key]; isExist == true {
+		l.queue.MoveToFront(elem)
+		elem.Value.(*Item).value = value
+
+		return
+		//elem.Value.(*Item).timestamp = time.Now()
+	}
+
+	if l.queue.Len() == l.capacity {
+		l.purge()
+	}
+
+	item := &Item{
+		key:   key,
+		value: value,
+		//timestamp: time.Now(),
+	}
+
+	elem := l.queue.PushFront(item)
+	l.items[key] = elem
 }
 
 func (l *lruCache) Range(f func(key, value int) bool) {
-	sortedStruct := make([]*Item, 0, len(l.items))
+	for e := l.queue.Back(); e != nil; e = e.Prev() {
+		if !f(e.Value.(*Item).key, e.Value.(*Item).value) {
+			return
+		}
+	}
 
-	for key, val := range l.items {
-		sortedStruct = append(sortedStruct, &Item{
-			key:       key,
-			value:     val.Value.(*Item).value,
-			timestamp: val.Value.(*Item).timestamp,
+	/*	sortedStruct := make([]*Item, 0, len(l.items))
+
+		for key, val := range l.items {
+			sortedStruct = append(sortedStruct, &Item{
+				key:       key,
+				value:     val.Value.(*Item).value,
+				timestamp: val.Value.(*Item).timestamp,
+			})
+		}
+
+		sort.Slice(sortedStruct, func(i, j int) bool {
+			return sortedStruct[i].timestamp.Before(sortedStruct[j].timestamp)
 		})
-	}
 
-	sort.Slice(sortedStruct, func(i, j int) bool {
-		return sortedStruct[i].timestamp.Before(sortedStruct[j].timestamp)
-	})
-
-	for _, item := range sortedStruct {
-		f(item.key, item.value)
-	}
+		for _, item := range sortedStruct {
+			f(item.key, item.value)
+		}*/
 }
 
 func (l *lruCache) Clear() {
-	for len(l.items) > 0 {
-		l.purge()
-	}
+	l.queue = list.New()
 }
 
 func (l *lruCache) purge() {
